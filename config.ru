4b3b25180fdf257f117ad38815c46f73f877a1f5
Andy Piper <andypiperuk@gmail.com>
2014-01-23 10:36:58 +0000

    Nice config.ru is nice

---
+++ config.ru
@@ -0,0 +1,3 @@
+require './mqtt_message_server'
+
+run MQTTServer.new
--- mqtt_message_server.rb
+++ mqtt_message_server.rb
@@ -1,23 +1,26 @@
 require 'sinatra'
 require 'mqtt'
 
-set :public_folder, File.dirname(__FILE__) + '/public'
+class MQTTServer < Sinatra::Base
+  set :public_folder, File.dirname(__FILE__) + '/public'
 
-get '/' do
-  erb :index
-end
+  get '/' do
+    erb :index
+  end
 
-get '/readings' do
-  headers 'Content-Type' => 'text/event-stream'
-  headers 'Cache-Control'=> 'no-cache'
-  headers 'Connection' => 'keep-alive'
-  stream do |out|
-    MQTT::Client.connect('m2m.eclipse.org') do |c|
-      c.get('PowerMeter/#') do |topic, message|
-        name = topic.split("/")[1]
-        out << "event: #{name}\n"
-        out << "data: #{message}\n\n"
+  get '/readings' do
+    headers 'Content-Type' => 'text/event-stream'
+    headers 'Cache-Control'=> 'no-cache'
+    headers 'Connection' => 'keep-alive'
+    stream do |out|
+      MQTT::Client.connect('m2m.eclipse.org') do |c|
+        c.get('PowerMeter/#') do |topic, message|
+          name = topic.split("/")[1]
+          out << "event: #{name}\n"
+          out << "data: #{message}\n\n"
+        end
       end
     end
   end
+
 end
